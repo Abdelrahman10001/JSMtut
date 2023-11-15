0f8b24c290c6f58791ba33ba44d497f9e2340b83
AbdelrahmanSaad <abdosaad0900@gmail.com>
2023-11-15 17:31:37 +0200

    metadata added

--- app/(root)/(home)/loading.tsx
+++ app/(root)/(home)/loading.tsx
@@ -4,14 +4,14 @@ const loading = () => {
   return <>
     <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
       <section className="nav-padding w-full">
-        <Skeleton className=" bg-gray-500 h-[274px] w-full rounded-lg" />
+        <Skeleton className=" bg-black-200/40 h-[274px] w-full rounded-lg" />
       </section>
       <section className="mt-6 flex w-full flex-col sm:mt-20">
-        <Skeleton className="h-10 w-56 bg-gray-500" />
+        <Skeleton className="h-10 w-56 bg-black-200/40" />
         <div className="mt-12 w-full flex flex-wrap justify-center gap-16 sm:justify-start">
-          <Skeleton className="h-[440px] w-full sm:w-[356] bg-gray-500" />
-          <Skeleton className="h-[440px] w-full sm:w-[356] bg-gray-500" />
-          <Skeleton className="h-[440px] w-full sm:w-[356] bg-gray-500" />
+          <Skeleton className="h-[440px] w-full sm:w-[356] bg-black-200/40" />
+          <Skeleton className="h-[440px] w-full sm:w-[356] bg-black-200/40" />
+          <Skeleton className="h-[440px] w-full sm:w-[356] bg-black-200/40" />
         </div>
       </section>
     </main>
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,16 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'JSM',
-  description: 'Optimize Your JSM',
+  description: 'JSM Tutorial',
+  other: {
+    'theme-color': '#0d1117',
+    "color-scheme": "dark only",
+    "twitter:image": 'https://i.ibb.co/d6TXxB2/homepage-thumnail.jpg',
+    "twitter:card": "summary_large-image",
+    "og:url": "abdosaad.pro",
+    "og:image": "https://i.ibb.co/d6TXxB2/homepage-thumnail.jpg",
+    "og:type": "website",
+  }
 }
 
 export default function RootLayout({
